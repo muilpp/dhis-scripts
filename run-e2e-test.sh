1d32ce9b4a47e5d4b7a3c63f4066237274e12879
Marc <marc@dhis2.org>
2024-08-16 11:03:45 +0200

    Parameterize test name on run e2e test script

--- run-e2e-test.sh
+++ run-e2e-test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd ~/workspace/dhis/dhis2-core/dhis-2/dhis-test-e2e
-mvn test -Dinstance.url=http://localhost:$1/api -Dtest.cleanup=true -Duser.default.username=admin -Duser.default.password=district -Dtest.track_called_endpoints=true -Dtest=org.hisp.dhis.tracker.imports.OwnershipTests#shouldValidateEnrollmentOwnership
+mvn test -Dinstance.url=http://localhost:$1/api -Dtest.cleanup=true -Duser.default.username=admin -Duser.default.password=district -Dtest.track_called_endpoints=true -Dtest=$2
+#mvn test -Dinstance.url=http://localhost:$1/api -Dtest.cleanup=true -Duser.default.username=admin -Duser.default.password=district -Dtest.track_called_endpoints=true -Dtest=org.hisp.dhis.tracker.imports.OwnershipTests#shouldValidateEnrollmentOwnership
